41f88d11f0b39619af1cd7a2436bfb719a8b269e
goatmanwaffles <goatmanwaffles@gmail.com>
2026-04-01 21:02:15 -0400

    Added login page

--- mealprepAssistant/src/routes/+page.svelte
+++ mealprepAssistant/src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<h1 class="flex justify-center mt-8 ">MealPrep Assistant</h1>
+
+<!-- Login Box-->
+<div class="flex justify-center justify-content-center bg-gray-400 rounded-xl flex-col">
+    <h2>Login</h2>
+    <form class="flex flex-col" method="POST">
+        <label for="email">Enter your Email</label>
+        <input type="email">
+
+        <label for="password">Enter your Password</label>
+        <input type="password">
+
+        <input type="submit">
+    </form>
+</div>
